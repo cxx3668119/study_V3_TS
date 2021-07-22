7d183eb6a70219762440ca3f5a395f511fae2df6
cxx3668119 <862704268@qq.com>
2021-07-22 10:55:03 +0800

    animate

---
+++ 09_learn_animation/src/03_结合三方库/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div><button @click="isShow = !isShow">显示/隐藏</button></div>
+    <transition name="cxx">
+      <h2 class="title" v-if="isShow">hello world</h2>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.title {
+  display: inline-block;
+}
+.cxx-enter-active {
+  animation: bounceInUp 1s ease-in;
+}
+
+.cxx-leave-active {
+  animation: bounceInUp 1s ease-in reverse;
+}
+</style>
--- 09_learn_animation/src/main.js
+++ 09_learn_animation/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './02_inout/App.vue'
+import App from './03_结合三方库/App.vue'
+import "animate.css"
+
 
 createApp(App).mount('#app')
